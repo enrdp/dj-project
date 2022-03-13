4851644f51539a73958ffb53a07c17ca3b03f408
enrdp <enr.delpriore@gmail.com>
2022-03-13 23:01:03 +0100

    update

--- js/zoom.js
+++ js/zoom.js
@@ -8,6 +8,7 @@ Array.from(document.querySelectorAll(".gallery__img")).forEach(item => {
       document.body.style.overflow = "hidden";
       modalImage.src = event.target.src;
       document.getElementById('scrollToTopBtn').style.zIndex = "0";
+      document.getElementById('menu').style.zIndex = "0";
       if(event.target == zoom1){
         modalImage.src = "./images/gallery-1-original.jpg";
       }else if(event.target == zoom2){
@@ -24,5 +25,6 @@ Array.from(document.querySelectorAll(".gallery__img")).forEach(item => {
 document.querySelector(".close").addEventListener("click", () => {
    modalEle.style.display = "none";
    document.getElementById('scrollToTopBtn').style.zIndex = "900";
+   document.getElementById('menu').style.zIndex = "999";
    document.body.style.overflow = "scroll";
 });
